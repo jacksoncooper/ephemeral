081bd77497f25f8570a103b590a9e9b880b55949
Jackson Cooper <jacksoncooper@users.noreply.github.com>
2021-02-27 22:17:37 -0800

    Messy Kindle annotation rehydrator.

--- src/Kindle.hs
+++ src/Kindle.hs
@@ -1,9 +1,9 @@
 module Kindle where
 
-import Data.List (stripPrefix)
+import Data.List (isPrefixOf, stripPrefix)
 import Text.Read (readMaybe)
 
-linesOfMetadata = 7
+linesOfMetadata = 8
 
 data Color = Pink | Blue | Yellow | Orange
   deriving Show
@@ -12,17 +12,17 @@ type Location = Int
 type Starred = Bool
 type Annotation = String
 
-data Highlight = Highlight Color Location Starred Annotation
-  deriving Show
-
-between :: Eq a => a -> a -> [a] -> [a]
-between start end =
-  takeWhile (/= end) . drop 1 . dropWhile (/= start)
+data Highlight =
+  Highlight
+    (Maybe Color)
+    (Maybe Location)
+    (Maybe Starred)
+    Annotation
+    deriving Show
 
 toColor :: String -> Maybe Color
 toColor text =
-  let color = between '(' ')' text
-  in case color of
+  case inParentheses text of
     "Pink"   -> Just Pink
     "Blue"   -> Just Blue
     "Yellow" -> Just Yellow
@@ -33,15 +33,50 @@ toLocation :: String -> Maybe Int
 toLocation location =
   stripPrefix "Location " location >>= readMaybe
 
-toStarred :: String -> Bool
-toStarred = (== "Y")
+toStarred :: String -> Maybe Bool
+toStarred text =
+  case text of
+    "Y" -> Just True
+    ""  -> Just False
+    _   -> Nothing
 
-toHighlight :: String -> Highlight
-toHighlight line = undefined
+isHighlight :: String -> Bool
+isHighlight = isPrefixOf "Highlight" . drop 1
+
+toHighlight :: String -> Maybe Highlight
+toHighlight line =
+  -- This is very bad.
+  let attributes = split ',' line in
+  if isHighlight line && length attributes == 4
+  then Just $
+    Highlight
+      (toColor . inQuotes $ attributes !! 0)
+      (toLocation . inQuotes $ attributes !! 1)
+      (toStarred . inQuotes $ attributes !! 2)
+      (inQuotes $ attributes !! 3)
+  else Nothing
+
+readHighlights :: FilePath -> IO [Maybe Highlight]
+readHighlights highlights =
+  (map toHighlight . toLines) <$> readFile highlights
 
 toLines :: String -> [String]
 toLines = drop linesOfMetadata . lines
 
-readHighlights :: FilePath -> IO [Highlight]
-readHighlights highlights =
-  (map toHighlight) <$> toLines <$> readFile highlights
+between :: Eq a => a -> a -> [a] -> [a]
+between start end =
+  takeWhile (/= end) . drop 1 . dropWhile (/= start)
+
+inParentheses :: String -> String
+inParentheses = between '(' ')'
+
+inQuotes :: String -> String
+inQuotes = between '"' '"'
+
+split :: Char -> String -> [String]
+split character text =
+  -- TODO: This destroys highlights that have commas in them because Amazon
+  -- doesn't sanitize the annotations export.
+  let (head, rest) = drop 1 <$> break (== ',') text in
+  if head == "" then []
+  else head : split character rest
