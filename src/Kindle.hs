1d8e1ce0bf977be63d9130ee6a8aa5573e44dd2d
Jackson Cooper <jacksoncooper@users.noreply.github.com>
2021-03-01 05:15:01 +0000

    Support note annotations.

--- src/Kindle.hs
+++ src/Kindle.hs
@@ -1,24 +1,44 @@
 module Kindle where
 
+import Data.Char (toLower)
 import Data.List (isPrefixOf, stripPrefix)
-import Text.Read (readMaybe)
 
-linesOfMetadata = 8
+type Author = String
+type Title = String
+
+type Location = String
+type Starred = Bool
+type Excerpt = String
+
+type Annotation = Either Highlight Note
 
 data Color = Pink | Blue | Yellow | Orange
   deriving Show
 
-type Location = Int
-type Starred = Bool
-type Annotation = String
+data Export =
+  Export
+    (Maybe Author)
+    (Maybe Title)
+    [Annotation]
+  deriving Show
 
 data Highlight =
   Highlight
     (Maybe Color)
-    (Maybe Location)
+    Location
     (Maybe Starred)
-    Annotation
-    deriving Show
+    Excerpt
+  deriving Show
+
+data Note =
+  Note
+    Location
+    (Maybe Starred)
+    Excerpt
+  deriving Show
+
+toAnnotationType :: String -> String
+toAnnotationType = map toLower . takeWhile (/= ' ')
 
 toColor :: String -> Maybe Color
 toColor text =
@@ -29,10 +49,6 @@ toColor text =
     "Orange" -> Just Orange
     _        -> Nothing
 
-toLocation :: String -> Maybe Int
-toLocation location =
-  stripPrefix "Location " location >>= readMaybe
-
 toStarred :: String -> Maybe Bool
 toStarred text =
   case text of
@@ -40,34 +56,31 @@ toStarred text =
     ""  -> Just False
     _   -> Nothing
 
-isHighlight :: String -> Bool
-isHighlight = isPrefixOf "Highlight" . drop 1
-
-toHighlight :: String -> Maybe Highlight
-toHighlight line =
+toAnnotation :: String -> Maybe Annotation
+toAnnotation line =
   let attributes = columns line in
-  if isHighlight line && length attributes == 4
-  then Just $
-    Highlight
-      (toColor $ attributes !! 0)
-      (toLocation $ attributes !! 1)
-      (toStarred $ attributes !! 2)
-      (attributes !! 3)
-  else Nothing
-
-readHighlights :: FilePath -> IO [Maybe Highlight]
-readHighlights highlights =
-  (map toHighlight . toLines) <$> readFile highlights
+  if length attributes == 4 then
+    let
+      color = toColor $ attributes !! 0
+      annotationType = toAnnotationType $ attributes !! 0
+      location = attributes !! 1
+      starred = toStarred $ attributes !! 2
+      excerpt = attributes !! 3
+    in case annotationType of
+      "highlight" -> Just (Left (Highlight color location starred excerpt))
+      "note"      -> Just (Right (Note location starred excerpt))
+      _           -> Nothing
+  else
+    Nothing
 
 toLines :: String -> [String]
-toLines = drop linesOfMetadata . lines
+toLines =
+  let linesOfMetadata = 8 in
+  drop linesOfMetadata . lines
 
-between :: Eq a => a -> a -> [a] -> [a]
-between start end =
-  takeWhile (/= end) . drop 1 . dropWhile (/= start)
-
-inParentheses :: String -> String
-inParentheses = between '(' ')'
+readAnnotations :: FilePath -> IO [Maybe Annotation]
+readAnnotations highlights =
+  (map toAnnotation . toLines) <$> readFile highlights
 
 columns :: String -> [String]
 columns = reverse . columns' . reverse 
@@ -87,3 +100,10 @@ columns' = go False [] '"'
         if consume
         then go True (head : column) toggle rest
         else go False [] toggle rest
+
+between :: Eq a => a -> a -> [a] -> [a]
+between start end =
+  takeWhile (/= end) . drop 1 . dropWhile (/= start)
+
+inParentheses :: String -> String
+inParentheses = between '(' ')'
