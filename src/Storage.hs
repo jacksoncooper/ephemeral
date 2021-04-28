6c902960a3c8b0a6a2042b8afe1b7cca33353bb3
Jackson Cooper <jacksoncooper@users.noreply.github.com>
2021-04-27 21:48:03 -0700

    Catch attempts to read from nonexistent file.

--- src/Storage.hs
+++ src/Storage.hs
@@ -3,21 +3,30 @@ module Storage
   , save
   ) where
 
+import Control.Exception (IOException, catch)
 import Data.Aeson (decodeFileStrict, encodeFile)
 import qualified Word as W
 
 store :: String
 store = "highlights.json"
 
-save :: [W.Word] -> IO ()
+save :: [W.Word] -> IO Bool
 save words =
-  encodeFile store words
+  doWrite `catch` badWrite
+  where
+    doWrite = encodeFile store words >> return True
 
--- TODO:
---   - 'decodeFileStrict' throws an exception when the file does not exist.
---   - Display the error message in Aeson's parser to the user.
---     Use 'eitherDecodeFileStrict'.
+badWrite :: IOException -> IO Bool
+badWrite _ = return False
+
+-- TODO: Display the error message in Aeson's parser to the user.
+-- Use 'eitherDecodeFileStrict'.
 
 load :: IO (Maybe [W.Word])
 load =
-  decodeFileStrict store
+  doRead `catch` badRead
+  where
+    doRead = decodeFileStrict store
+
+badRead :: IOException -> IO (Maybe [W.Word])
+badRead _ = return (Just [])
